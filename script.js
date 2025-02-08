8aa6f7b15b770854243f91d44d78a53322f6f8d2
camila <mariacamilaluango@gmail.com>
2025-02-07 19:11:01 -0500

    agregamos ciclo for

---
+++ script.js
@@ -0,0 +1,10 @@
+const ofertas = [1500, 2000, 2500, 3000, 3500, 4000];
+const salarioMinimo = 2500;
+
+function buscarEmpleo (eferta, salarioMinimo){
+    for (let i = 0; i >= salarioMinimo; i++){
+        
+
+    }
+    
+}
