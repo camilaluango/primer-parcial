2c099da6bdffd1a2a79988d3468fdad1c3bff152
camila <mariacamilaluango@gmail.com>
2025-02-07 19:45:54 -0500

    comentarios

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
+// usamos busqueda lineal
 const ofertas = [1500, 2000, 2500, 3000, 3500, 4000];
-const salarioMinimo = 1000;
+const salarioMinimo = 2700;
 
+// se buscan las primeras ofertas
 function buscarPrimeraOferta (ofertas, salarioMinimo){
 
     for (let i = 0; i < ofertas.length; i++){
-        if(ofertas[i] >= salarioMinimo){
-            return i;
+        if(ofertas[i] >= salarioMinimo){ //compara oferta correcta
+            return i; //retomamos el indice de la primera oferta valida
             
         }
 
     }
-    return -1;
+    return -1; //si no se encuentra niguna oferta que cumpla, arrojara -1
     
 }
 
